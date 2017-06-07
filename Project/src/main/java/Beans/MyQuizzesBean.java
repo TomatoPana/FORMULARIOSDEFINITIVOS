1dc17b55dd89c7601dfbe9221a54c6e408123c42
Iván Morett <luisivanmorett@gmail.com>
2017-06-07 17:43:38 -0500

    Ya funciona lo que queria del dash
    Falta que neta se agreguen encuestas y se editen atributos de ellas y eso xD

--- Project/src/main/java/Beans/MyQuizzesBean.java
+++ Project/src/main/java/Beans/MyQuizzesBean.java
@@ -7,7 +7,10 @@ package Beans;
 
 import Database.Categories;
 import Database.Quiz;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -31,16 +34,25 @@ public class MyQuizzesBean {
     
     @PostConstruct
     public void initialize() {
-        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Persistence" );
-        EntityManager entitymanager = emfactory.createEntityManager();
         FacesContext context = FacesContext.getCurrentInstance();
         Integer userId = (Integer) context.getExternalContext().getSessionMap().get("id");
-        Query query = entitymanager.createNamedQuery("Quiz.findByUserId", Categories.class);
-        query.setParameter("userId", userId);
-        lista = query.getResultList();
-        entitymanager.close();
-        selectedQuiz=new Database.Quiz();
-        selectedQuiz.setId(-1);
+        if(userId != null)
+        {
+            EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Persistence" );
+            EntityManager entitymanager = emfactory.createEntityManager();
+            Query query = entitymanager.createNamedQuery("Quiz.findByUserId", Quiz.class);
+            query.setParameter("userId", userId);
+            lista = query.getResultList();
+            entitymanager.close();
+        }
+        else
+        {
+            try {
+                context.getExternalContext().redirect("/forms");
+            } catch (IOException ex) {
+                Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     public List<Quiz> getLista() {
--- Project/target/generated-sources/annotations/Database/Answer_.java
+++ Project/target/generated-sources/annotations/Database/Answer_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-07T07:29:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-07T17:40:37")
 @StaticMetamodel(Answer.class)
 public class Answer_ { 
 
