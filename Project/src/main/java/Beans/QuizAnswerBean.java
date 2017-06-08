adee8d1975fd0d10d7749b1f00e06127e1f1177d
Iván Morett <luisivanmorett@gmail.com>
2017-06-07 23:29:20 -0500

    Completamente funcional contestar respuestas
    Pues eso, con un id de encuesta ya se genera en el mismo index la encuesta para ir contestando :D

---
+++ Project/src/main/java/Beans/QuizAnswerBean.java
@@ -0,0 +1,212 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import Database.Answer;
+import Database.Options;
+import Database.Questions;
+import Database.Quiz;
+import Database.UserAnswer;
+import Database.Users;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import org.primefaces.extensions.model.dynaform.DynaFormControl;
+import org.primefaces.extensions.model.dynaform.DynaFormLabel;
+import org.primefaces.extensions.model.dynaform.DynaFormModel;
+import org.primefaces.extensions.model.dynaform.DynaFormRow;
+
+/**
+ *
+ * @author Luis Iván Morett
+ */
+@ManagedBean
+@ViewScoped
+public class QuizAnswerBean {
+    
+    private Integer id;
+    private String email;
+    private String type;
+    private List options;
+    private List<Questions> questions;
+    private Integer conteo;
+    private Integer cantidad;
+    private Database.Questions now;
+    private String answer;
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List getOptions() {
+        return options;
+    }
+
+    public void setOptions(List options) {
+        this.options = options;
+    }
+
+    public List<Questions> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Questions> questions) {
+        this.questions = questions;
+    }
+
+    public Integer getConteo() {
+        return conteo;
+    }
+
+    public void setConteo(Integer conteo) {
+        this.conteo = conteo;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Questions getNow() {
+        return now;
+    }
+
+    public void setNow(Questions now) {
+        this.now = now;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    
+    
+    /**
+     * Creates a new instance of DynaFormAnswersBean
+     */
+    public QuizAnswerBean() {
+    }
+    
+    public void selectedQuiz(int id)
+    {
+        this.id = id;
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Persistence" );
+        EntityManager entitymanager = emfactory.createEntityManager();
+        Query query = entitymanager.createNamedQuery("Questions.findByQuizId", Questions.class);
+        query.setParameter("quizId", id);
+        questions = query.getResultList();
+        cantidad = questions.size();
+        
+        conteo = 0;
+        query = entitymanager.createNamedQuery("Quiz.findById", Quiz.class);
+        query.setParameter("id", id);
+        Quiz valor = (Quiz) query.getSingleResult();
+        titulo = valor.getTitle();
+        
+        entitymanager.close();
+    }
+    public void next()
+    {
+        if(conteo>0)
+        {
+            EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Persistence" );
+            EntityManager entitymanager = emfactory.createEntityManager();
+            entitymanager.getTransaction().begin();
+            Answer answerEntity = new Answer();
+            answerEntity.setAnswer(answer);
+            entitymanager.persist(answerEntity);
+            entitymanager.flush();
+            
+            Integer answerId = answerEntity.getId();
+            UserAnswer userAnswer = new UserAnswer();
+            userAnswer.setAnswerId(answerId);
+            userAnswer.setQuestionId(now.getId());
+            userAnswer.setEmail(email);
+            entitymanager.persist(userAnswer);
+            entitymanager.flush();
+            entitymanager.getTransaction().commit();
+            entitymanager.close();
+            answer=null;
+
+        }  
+        if(conteo<cantidad)
+        {
+            
+            now = (Database.Questions)questions.get(conteo);
+            type=now.getType();
+            if(type.equals("options"))
+            {
+                EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Persistence" );
+                EntityManager entitymanager = emfactory.createEntityManager();
+                Query query = entitymanager.createNamedQuery("Options.findByIdQuestion", Options.class);
+                query.setParameter("idQuestion", now.getId());
+                options = query.getResultList();
+                entitymanager.close();
+            }
+            conteo++;
+        }
+        else
+        {
+            conteo++;
+        }
+    }
+    
+    public void end()
+    {
+        id=null;
+        email=null;
+        type=null;
+        options=null;
+        questions=null;
+        conteo=0;
+        now=null;
+        answer=null;
+    }
+    
+    
+}
--- Project/src/main/java/Database/UserAnswer.java
+++ Project/src/main/java/Database/UserAnswer.java
@@ -8,86 +8,97 @@ package Database;
 import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author USUARIO
+ * @author Luis Iván Morett
  */
 @Entity
 @Table(name = "user_answer")
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "UserAnswer.findAll", query = "SELECT u FROM UserAnswer u")
-    , @NamedQuery(name = "UserAnswer.findById", query = "SELECT u FROM UserAnswer u WHERE u.userAnswerPK.id = :id")
-    , @NamedQuery(name = "UserAnswer.findByQuestionId", query = "SELECT u FROM UserAnswer u WHERE u.questionId = :questionId")
     , @NamedQuery(name = "UserAnswer.findByAnswerId", query = "SELECT u FROM UserAnswer u WHERE u.answerId = :answerId")
-    , @NamedQuery(name = "UserAnswer.findByEmail", query = "SELECT u FROM UserAnswer u WHERE u.userAnswerPK.email = :email")})
+    , @NamedQuery(name = "UserAnswer.findByQuestionId", query = "SELECT u FROM UserAnswer u WHERE u.questionId = :questionId")
+    , @NamedQuery(name = "UserAnswer.findById", query = "SELECT u FROM UserAnswer u WHERE u.id = :id")
+    , @NamedQuery(name = "UserAnswer.findByEmail", query = "SELECT u FROM UserAnswer u WHERE u.email = :email")})
 public class UserAnswer implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @EmbeddedId
-    protected UserAnswerPK userAnswerPK;
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "question_id")
-    private int questionId;
-    @Basic(optional = false)
-    @NotNull
     @Column(name = "answer_id")
-    private int answerId;
+    private Integer answerId;
+    @Column(name = "question_id")
+    private Integer questionId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "email")
+    private String email;
 
     public UserAnswer() {
     }
 
-    public UserAnswer(UserAnswerPK userAnswerPK) {
-        this.userAnswerPK = userAnswerPK;
+    public UserAnswer(Integer id) {
+        this.id = id;
     }
 
-    public UserAnswer(UserAnswerPK userAnswerPK, int questionId, int answerId) {
-        this.userAnswerPK = userAnswerPK;
-        this.questionId = questionId;
-        this.answerId = answerId;
+    public UserAnswer(Integer id, String email) {
+        this.id = id;
+        this.email = email;
     }
 
-    public UserAnswer(int id, String email) {
-        this.userAnswerPK = new UserAnswerPK(id, email);
-    }
-
-    public UserAnswerPK getUserAnswerPK() {
-        return userAnswerPK;
-    }
-
-    public void setUserAnswerPK(UserAnswerPK userAnswerPK) {
-        this.userAnswerPK = userAnswerPK;
-    }
-
-    public int getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(int questionId) {
-        this.questionId = questionId;
-    }
-
-    public int getAnswerId() {
+    public Integer getAnswerId() {
         return answerId;
     }
 
-    public void setAnswerId(int answerId) {
+    public void setAnswerId(Integer answerId) {
         this.answerId = answerId;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (userAnswerPK != null ? userAnswerPK.hashCode() : 0);
+        hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
 
@@ -98,7 +109,7 @@ public class UserAnswer implements Serializable {
             return false;
         }
         UserAnswer other = (UserAnswer) object;
-        if ((this.userAnswerPK == null && other.userAnswerPK != null) || (this.userAnswerPK != null && !this.userAnswerPK.equals(other.userAnswerPK))) {
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
@@ -106,7 +117,7 @@ public class UserAnswer implements Serializable {
 
     @Override
     public String toString() {
-        return "Database.UserAnswer[ userAnswerPK=" + userAnswerPK + " ]";
+        return "Database.UserAnswer[ id=" + id + " ]";
     }
     
 }
